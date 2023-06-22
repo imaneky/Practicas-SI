68af547f22e7034fdc219670003e369b12fa3734
Imane Kadiri <imane.kadiri@gmail.com>
2023-06-22 20:24:25 +0200

    ejercicio 3 completado
    todos los apartados del ejercicio 3 + output ordenado

--- analisisDatos/ejercicio3.py
+++ analisisDatos/ejercicio3.py
@@ -13,6 +13,9 @@ join_tables_df = pd.read_sql_query('SELECT * FROM alerts JOIN devices ON (alerts
 ######################################
 # AGRUPACION POR PRIORIDAD DE ALERTA
 ######################################
+print("-------------------------------------------------------")
+print(" AGRUPACIONES POR PRIORIDAD")
+print("-------------------------------------------------------")
 
 alerts_p1 = join_tables_df.loc[join_tables_df['prioridad'] == 1]
 alerts_p2 = join_tables_df.loc[join_tables_df['prioridad'] == 2]
@@ -34,33 +37,108 @@ vuln_p3 = alerts_p3.loc[alerts_p3['analisisVulnerabilidades'] > 0]
 total_p3 = vuln_p3['analisisVulnerabilidades'].sum()
 
 print("Nº de vulnerabilidades detectadas en alertas P1: ", total_p1)
-print("Nº de vulnerabilidades detectadas en alertas P1: ", total_p2)
-print("Nº de vulnerabilidades detectadas en alertas P1: ", total_p3)
+print("Nº de vulnerabilidades detectadas en alertas P2: ", total_p2)
+print("Nº de vulnerabilidades detectadas en alertas P3: ", total_p3)
 
-print("\n")
+print("-------------------------------------------------------")
 
 # Nº VALORES AUSENTES
 
+total_valores_null_p1 = 0
+for columna in alerts_p1.columns:
+    if alerts_p1[columna].eq("NULL").any():
+        total_valores_null_p1 += alerts_p1[columna].eq("NULL").sum()
 
-print("Nº valores ausentes: ")
-print("Nº valores ausentes en P1: ")
-print("Nº valores ausentes en P2: ")
-print("Nº valores ausentes en P2: ")
+total_valores_null_p2 = 0
+for columna in alerts_p2.columns:
+    if alerts_p2[columna].eq("NULL").any():
+        total_valores_null_p2 += alerts_p2[columna].eq("NULL").sum()
 
+total_valores_null_p3 = 0
+for columna in alerts_p3.columns:
+    if alerts_p3[columna].eq("NULL").any():
+        total_valores_null_p3 += alerts_p3[columna].eq("NULL").sum()
+
+print("Nº valores ausentes: ", total_valores_null_p3+total_valores_null_p2+total_valores_null_p1)
+print("Nº valores ausentes en P1: ", total_valores_null_p1)
+print("Nº valores ausentes en P2: ", total_valores_null_p2)
+print("Nº valores ausentes en P2: ", total_valores_null_p3)
+print("-------------------------------------------------------")
+"""
+total_valores_null = 0
+for valor in join_tables_df['localizacion']:
+    if valor == "NULL":
+        total_valores_null += 1
+
+print(total_valores_null)
+"""
 # MODA
+
+moda_p1 = alerts_p1['analisisVulnerabilidades']
+frecuencia_p1 = moda_p1.value_counts()
+mas_frecuente_p1 = frecuencia_p1.idxmax()
+repeticiones_p1 = frecuencia_p1[mas_frecuente_p1]
+
+moda_p2 = alerts_p2['analisisVulnerabilidades']
+frecuencia_p2 = moda_p2.value_counts()
+mas_frecuente_p2 = frecuencia_p2.idxmax()
+repeticiones_p2 = frecuencia_p2[mas_frecuente_p2]
+
+moda_p3 = alerts_p3['analisisVulnerabilidades']
+frecuencia_p3 = moda_p3.value_counts()
+mas_frecuente_p3 = frecuencia_p3.idxmax()
+repeticiones_p3 = frecuencia_p3[mas_frecuente_p3]
+
+print("La moda de las vulnerabilidades detectadas de P1: ", mas_frecuente_p1, "con ", repeticiones_p1, "repeticiones")
+print("La moda de las vulnerabilidades detectadas de P1: ", mas_frecuente_p2, "con ", repeticiones_p2, "repeticiones")
+print("La moda de las vulnerabilidades detectadas de P1: ", mas_frecuente_p3, "con ", repeticiones_p3, "repeticiones")
+print("-------------------------------------------------------")
 
 # MEDIANA
 
+mediana_prioridad = join_tables_df.groupby('prioridad')['analisisVulnerabilidades'].median()
+print("La mediana por prioridad es: ", mediana_prioridad)
+
+print("-------------------------------------------------------")
+
 # CUARTILES Q1 Y Q3
+
+quartiles = alerts_p1['analisisVulnerabilidades'].quantile([0.25, 0.5, 0.75])
+print("Cuartiles P1:")
+print("Q1:", quartiles[0.25])
+print("Q2 (Mediana):", quartiles[0.5])
+print("Q3:", quartiles[0.75])
+
+quartiles = alerts_p2['analisisVulnerabilidades'].quantile([0.25, 0.5, 0.75])
+print("Cuartiles P2:")
+print("Q1:", quartiles[0.25])
+print("Q2 (Mediana):", quartiles[0.5])
+print("Q3:", quartiles[0.75])
+
+quartiles = alerts_p3['analisisVulnerabilidades'].quantile([0.25, 0.5, 0.75])
+print("Cuartiles P3:")
+print("Q1:", quartiles[0.25])
+print("Q2 (Mediana):", quartiles[0.5])
+print("Q3:", quartiles[0.75])
 
 
 # VALORES MAX. Y MIN.
 
+print(f"El nº minimo de vulnerabilidades detectadas de P1 son: {alerts_p1['analisisVulnerabilidades'].min()}")
+print(f"El nº maximo de vulnerabilidades detectadas de P1 son: {alerts_p1['analisisVulnerabilidades'].max()}")
+print(f"El nº minimo de vulnerabilidades detectadas de P2 son: {alerts_p2['analisisVulnerabilidades'].min()}")
+print(f"El nº maximo de vulnerabilidades detectadas de P2 son: {alerts_p2['analisisVulnerabilidades'].max()}")
+print(f"El nº minimo de vulnerabilidades detectadas de P3 son: {alerts_p3['analisisVulnerabilidades'].min()}")
+print(f"El nº maximo de vulnerabilidades detectadas de P3 son: {alerts_p3['analisisVulnerabilidades'].max()}")
 
+print("\n")
 
 ######################################
 # POR FECHA: JULIO | AGOSTO
 ######################################
+print("-------------------------------------------------------")
+print(" AGRUPACIONES POR FECHA: JULIO Y AGOSTO")
+print("-------------------------------------------------------")
 
 join_tables_df['timestamp'] = pd.to_datetime(join_tables_df['timestamp'], format = '%Y-%m-%d %H:%M:%S')
 julio_df = join_tables_df[(join_tables_df['timestamp'].dt.month == 7)]
@@ -80,33 +158,80 @@ total_agosto = vuln_agosto['analisisVulnerabilidades'].sum()
 print("Nº vulnerabilidades detectadas en julio: ", total_julio)
 print("Nº vulnerabilidades detectadas en agosto: ", total_agosto)
 
+print("-------------------------------------------------------")
+
 # Nº VALORES AUSENTES
 
-print("Nº valores ausentes: ")
-print("Nº valores ausentes del mes de julio: ")
-print("Nº valores ausentes del mes de agosto: ")
+total_valores_null = 0
+for columna in join_tables_df.columns:
+    if join_tables_df[columna].eq("NULL").any():
+        total_valores_null += join_tables_df[columna].eq("NULL").sum()
+
+total_valores_null_julio = 0
+for columna in julio_df.columns:
+    if julio_df[columna].eq("NULL").any():
+        total_valores_null_julio += julio_df[columna].eq("NULL").sum()
+
+total_valores_null_agosto = 0
+for columna in agosto_df.columns:
+    if agosto_df[columna].eq("NULL").any():
+        total_valores_null_agosto += agosto_df[columna].eq("NULL").sum()
+
+print("Nº valores ausentes del mes de julio: ", total_valores_null_julio)
+print("Nº valores ausentes del mes de agosto: ", total_valores_null_agosto)
+print("-------------------------------------------------------")
 
 
 # MODA
 
+moda_julio = julio_df['analisisVulnerabilidades']
+frecuencia_julio = moda_julio.value_counts()
+mas_frecuente_julio = frecuencia_julio.idxmax()
+repeticiones_julio = frecuencia_julio[mas_frecuente_julio]
+
+moda_agosto = agosto_df['analisisVulnerabilidades']
+frecuencia_agosto = moda_agosto.value_counts()
+mas_frecuente_agosto = frecuencia_agosto.idxmax()
+repeticiones_agosto = frecuencia_agosto[mas_frecuente_agosto]
+
+print("La moda de las vulnerabilidades detectadas en julio es:", mas_frecuente_julio, "con ", repeticiones_julio, "repeticiones")
+print("La moda de las vulnerabilidades detectadas en agosto es:", mas_frecuente_agosto, "con ", repeticiones_agosto, "repeticiones")
+print("*Entendemos por repeticiones al numero de alertas que tienen 15 vulnerabilidades detectadas, y no al número total de vulnerabilidades.")
+print("-------------------------------------------------------")
+
 # MEDIANA
+
+mediana_julio =join_tables_df.loc[join_tables_df['timestamp'].dt.month.isin([7]), 'analisisVulnerabilidades'].median()
+mediana_agosto =join_tables_df.loc[join_tables_df['timestamp'].dt.month.isin([8]), 'analisisVulnerabilidades'].median()
+
+print("La mediana agrupando por el mes de julio es: ", mediana_julio)
+print("La mediana agrupando por el mes de agosto es: ", mediana_agosto)
+
+print("-------------------------------------------------------")
+
+
 
 # CUARTILES Q1 Y Q3
 
 
+quartiles = julio_df['analisisVulnerabilidades'].quantile([0.25, 0.5, 0.75])
+print("Cuartiles julio:")
+print("Q1:", quartiles[0.25])
+print("Q2 (Mediana):", quartiles[0.5])
+print("Q3:", quartiles[0.75])
+
+quartiles = agosto_df['analisisVulnerabilidades'].quantile([0.25, 0.5, 0.75])
+print("Cuartiles agosto:")
+print("Q1:", quartiles[0.25])
+print("Q2 (Mediana):", quartiles[0.5])
+print("Q3:", quartiles[0.75])
 
 # VALORES MAX. Y MIN.
 
-
-
-
-
-
-
-
-
-
-
+print(f"El nº minimo de vulnerabilidades detectadas en julio es:  {julio_df['analisisVulnerabilidades'].min()}")
+print(f"El nº maximo de vulnerabilidades detectadas en julio es: {julio_df['analisisVulnerabilidades'].max()}")
+print(f"El nº minimo de vulnerabilidades detectadas en agosto es: {agosto_df['analisisVulnerabilidades'].min()}")
+print(f"El nº maximo de vulnerabilidades detectadas en agosto es: {agosto_df['analisisVulnerabilidades'].max()}")
 
 
 conexion.close()
